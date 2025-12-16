9cfcf2b6c6e28dbcffb0af85560a1444bee1d517
ioana manghiuc <147768804+ioana-manghiuc@users.noreply.github.com>
2025-12-16 10:46:33 +0200

    + Created frontend Avalonia .NET MVVM App

---
+++ frontend/frontend/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace frontend.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ frontend/frontend/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace frontend.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
